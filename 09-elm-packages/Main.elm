46f2f63f7524e5b1460fd5fc793d487d6825d049
Andrew Jarrett <ahrjarrett@gmail.com>
2016-12-30 23:16:31 -0700

    finished course

--- 09-elm-packages/Main.elm
+++ 09-elm-packages/Main.elm
@@ -1,16 +1,19 @@
--- no longer works, package not updated for elm 0.18
+-- no longer works, String.Extra package not updated for elm 0.18
 -- make your own use of a String-related package here:
 
 module Main exposing (..)
 
 import Html exposing (..)
+import String.Extra exposing (pluralize)
+
 
 items =
   [ "Saffron", "Ratatouille" ]
 
 main =
   div []
-      [ h1 [] [ text <| "Kitchen Items" ]
+      [ h1 [] [
+        text <| (pluralize "Item" "Items" (List.length items))  ]
       , text <| toString <| items
       ]
 
