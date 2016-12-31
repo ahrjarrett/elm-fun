2747ca26c25c3197859afe708d03ef723c2e3204
Andrew Jarrett <ahrjarrett@gmail.com>
2016-12-30 22:16:52 -0700

    06: mapped over List as html in browser

---
+++ 06-html/Main.elm
@@ -0,0 +1,39 @@
+module Main exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+type alias Ship =
+  { name : String
+  , model : String
+  , cost : Int
+  }
+
+ships =
+  [ { name = "Honda", cost = 151151 }
+  , { name = "Civic", cost = 23333 }
+  , { name = "RV", cost = 1000000 }
+  ]
+
+renderShip ship =
+  li []
+     [ text ship.name
+     , text ", "
+     , b []
+         [ text <| toString ship.cost ]
+      ]
+
+-- create Tuple skeleton : [(,)]
+renderShips ships =
+  div
+    [ style
+      [ ( "font-family", "-apple-system" )
+      , ( "padding", "1em" )
+      ]
+    ]
+    [ h1 [] [text "Ships" ]
+    , ul [] (List.map renderShip ships)
+    ]
+
+
+main = renderShips ships
+
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # elm-fun
 
-playing in elm
+- 01 render text to browser
+- 03 types
+- 04 data structures
+- 05 lists and functors
+- 06 render mapped data as html
